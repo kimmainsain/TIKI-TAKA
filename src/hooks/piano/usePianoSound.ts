08a693de0373e688fa31d956be282426a70aae1d
kim.mainsain <kmsid003@gmail.com>
2024-05-04 23:43:22 +0900

    Fix : 키보드 꾹 눌렀을 때 소리가 반복되어 나오는 경우를 해결

--- src/hooks/piano/usePianoSound.ts
+++ src/hooks/piano/usePianoSound.ts
@@ -1,21 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { WHITE_KEYMAP, BLACK_KEYMAP } from "../../constants/Piano";
 
 const combinedKeyMap = [...WHITE_KEYMAP, ...BLACK_KEYMAP];
 
-export const usePianoSound = () => {
+// activeKeys type
+interface ActiveKeys {
+  [key: string]: boolean;
+}
 
+export const usePianoSound = () => {
+  const [activeKeys, setActiveKeys] = useState<ActiveKeys>({});
+  
   // 키보드 핸들링
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      const keyEntry = combinedKeyMap.find((item) => item.key === event.key);
-      if (keyEntry) {
-        playSound(keyEntry.code);
+      if (!activeKeys[event.key]) {
+        const keyEntry = combinedKeyMap.find((item) => item.key === event.key);
+        if (keyEntry) {
+          playSound(keyEntry.code);
+          setActiveKeys((prevKeys) => ({ ...prevKeys, [event.key]: true }));
+          console.log(activeKeys);
+        }
+      }
+    };
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (activeKeys[event.key]) {
+        setActiveKeys((prevKeys) => ({ ...prevKeys, [event.key]: false }));
       }
     };
     window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [activeKeys]);
 
   // 마우스 클릭 핸들링
   const handleKeyClick = (key: string) => {

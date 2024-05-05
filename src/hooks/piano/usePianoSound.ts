b6c6956081b7a2ebbbd9a6a2c9d01424fcda32e3
kim.mainsain <kmsid003@gmail.com>
2024-05-05 18:16:41 +0900

    Feat : 볼륨 recoil 상태관리 및 useRef 업데이트 -> 볼륨조절기능 완성

--- src/hooks/piano/usePianoSound.ts
+++ src/hooks/piano/usePianoSound.ts
@@ -1,6 +1,19 @@
+import { volumeState } from "../../recoils/volumeState";
+import { useRecoilState } from "recoil";
+import { useRef, useEffect } from "react";
+
 export const usePianoSound = () => {
+  const [volume] = useRecoilState(volumeState);
+  const volumeRef = useRef(volume);
+
+  useEffect(() => {
+    volumeRef.current = volume;
+    console.log("Volume updated to", volume);
+  }, [volume]);
+
   const playSound = (note: string) => {
     const audio = new Audio(`./PianoMP3/${note}.mp3`);
+    audio.volume = volumeRef.current;
     audio.play();
   };
 
